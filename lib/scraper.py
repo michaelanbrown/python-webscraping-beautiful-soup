89a429b0cc1b988d9f044a3a46af64ab94689ad5
michaelanbrown <m_brownbrown@yahoo.com>
2025-01-15 15:42:27 -0500

    printing '.heading-financier

--- lib/scraper.py
+++ lib/scraper.py
@@ -6,3 +6,5 @@ headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
 
 doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.select('.heading-financier'))
